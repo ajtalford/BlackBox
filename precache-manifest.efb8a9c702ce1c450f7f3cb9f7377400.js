fc508b5076935fd882707842f171aa2d33f846cd
ajtalford <ajtalford@gmail.com>
2019-03-13 23:33:01 -0400

    Updates

---
+++ precache-manifest.efb8a9c702ce1c450f7f3cb9f7377400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84b7cc472a6ccf0b84d",
+    "url": "/BlackBox/static/js/runtime~main.e07207c5.js"
+  },
+  {
+    "revision": "d5aafb9e7a10a18b1306",
+    "url": "/BlackBox/static/js/main.3666cde7.chunk.js"
+  },
+  {
+    "revision": "e22b5b0b95d349a2ed03",
+    "url": "/BlackBox/static/js/2.89daa70c.chunk.js"
+  },
+  {
+    "revision": "d5aafb9e7a10a18b1306",
+    "url": "/BlackBox/static/css/main.921a049b.chunk.css"
+  },
+  {
+    "revision": "b8e064575a8cb9f36f55371f9f334258",
+    "url": "/BlackBox/index.html"
+  }
+];
