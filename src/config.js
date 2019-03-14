6e677831089558c6d5b7415879fcf60d150ef8f1
ajtalford <ajtalford@gmail.com>
2019-03-13 22:51:29 -0400

    Portfolio App done

---
+++ src/config.js
@@ -0,0 +1,23 @@
+// Configuration for TMDB
+// JSON https://api.themoviedb.org/3/configurationapi_key=f8f902f7a93c48cd252c62e0dff59917
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'f8f902f7a93c48cd252c62e0dff59917';
+
+// Images URLs:
+// http://image.tmdb.org/t/p/w780/sAzw6I1G9JUxm86KokIDdQeWtaq.jpg
+ 
+const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+//Backdrop sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = 'w1280';
+
+// Poster sizes: w92, w154, w185, w342, w500, w780, original
+const POSTER_SIZE = 'w500';
+
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE
+}
