d130671e97a8061f38c6b6b9a5a2896072d2f480
ajtalford <ajtalford@gmail.com>
2019-03-14 00:02:13 -0400

    fixed path to build

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/BlackBox/">
       <React.Fragment>
         <Header />
         <Switch>
